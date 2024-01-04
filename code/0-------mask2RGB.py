44afed33dedca97b984a500f75163ca9986a1370
Tianlong Ai <50650583+AI-Tianlong@users.noreply.github.com>
2024-01-04 11:10:49 +0800

    Update 0-------mask2RGB.py

--- code/0-------mask2RGB.py
+++ code/0-------mask2RGB.py
@@ -1,26 +1,38 @@
 import numpy as np
 from PIL import Image
 from tqdm import tqdm, trange
-# 第一次使用，请先 `pip install ATL_Tools`
 from ATL_Tools import mkdir_or_exist, find_data_list
 import os
 
-MASK_path = './dataset/potsdam/mask_labels/'
-RGB_path = 'dataset/potsdam/vis_labels'
+MASK_path = '/opt/AI-Tianlong/Datasets/LoveDA/ann_dir/val'
+RGB_path = '/opt/AI-Tianlong/Datasets/LoveDA/ann_dir/vis_val'
 mkdir_or_exist(RGB_path)
 
 label_lists = find_data_list(MASK_path, suffix='png')
 
 # 思路：三个通道分别乘 2 3 4，然后相加，得到一个新的通道，然后根据这个通道的值，来判断是哪个类别
-classes = ['Background', 'Building', 'di_ai_zhi_bei',
-         'Tree', 'car', 'bu_tou_shui_mian']
 
-platte = [[255, 0, 0], [0, 0, 255], [0, 255, 255],
-          [0, 255, 0], [255, 255, 0], [255, 255, 255]]
+# 是否包含0类，classes和palette里没包含
+reduce_zero_label = True
 
-platte = np.array(platte)
+classes = ('background', 'building', 'road', 'water', 'barren', 'forest',
+            'agricultural')
 
+palette = [[255, 255, 255], [255, 0, 0], [255, 255, 0], [0, 0, 255],
+            [159, 129, 183], [0, 255, 0], [255, 195, 128]]
+
+# palette = np.array(palette)
+
+if reduce_zero_label:
+    new_palette = [[0, 0, 0]] + palette
+    print(f"palette: {new_palette}")
+else:
+    new_palette = palette
+    print(f"palette: {new_palette}")
+
+new_palette = np.array(new_palette)
 for mask_label_path in tqdm(label_lists):
-    mask_label = np.array(Image.open(mask_label_path))
-    RGB_label = platte[mask_label]
+    mask_label = np.array(Image.open(mask_label_path)).astype(np.uint8)
+    # print(mask_label.shape)
+    RGB_label = new_palette[mask_label]
     Image.fromarray(RGB_label.astype(np.uint8)).save(os.path.join(RGB_path, os.path.basename(mask_label_path).replace('.png', '.png')))
