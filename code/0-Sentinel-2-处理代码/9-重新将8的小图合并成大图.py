451d2f18c1365b18d1c681e2931249d2ff460169
Tianlong Ai <50650583+AI-Tianlong@users.noreply.github.com>
2024-05-23 10:08:35 +0800

    Update 9-重新将8的小图合并成大图.py

--- code/0-Sentinel-2-处理代码/9-重新将8的小图合并成大图.py
+++ code/0-Sentinel-2-处理代码/9-重新将8的小图合并成大图.py
@@ -16,15 +16,15 @@ def main():
     crop_size = 5000
 
     # 小图标签的存储位置
-    img_patch_path = '/opt/AI-Tianlong/Datasets/ATL-ATLNongYe/ATL推理大图/黑河/推理结果-3类作物/推理结果-小图-mask'
+    img_patch_path = '/opt/AI-Tianlong/Datasets/ATL-ATLNongYe/ATL推理大图/牡丹江/推理结果-24类/推理结果-24-mask-小图'
     img_patch_list = find_data_list(img_patch_path, suffix='.png')
 
     # 合并后的大图的存储位置：
-    img_big_path = '/opt/AI-Tianlong/Datasets/ATL-ATLNongYe/ATL推理大图/黑河/推理结果-3类作物/推理结果-mask'
+    img_big_path = '/opt/AI-Tianlong/Datasets/ATL-ATLNongYe/ATL推理大图/牡丹江/推理结果-24类/推理结果-24-mask'
     mkdir_or_exist(img_big_path)
 
     # 原始的大图的存储位置：为了读取尺寸
-    ori_img_big_path = '/opt/AI-Tianlong/Datasets/ATL-ATLNongYe/ATL推理大图/黑河/要推理的images-矢量裁切'
+    ori_img_big_path = '/opt/AI-Tianlong/Datasets/ATL-ATLNongYe/ATL推理大图/牡丹江/要推理的images-矢量裁切'
     ori_img_big_list = find_data_list(ori_img_big_path, suffix='.tif')
 
     # 这里需要知道每一个大图的尺寸，才能去合并。
@@ -33,27 +33,40 @@ def main():
         # 读取最原始的大图，获取尺寸
         # 南岗区.tif
         img_basename = os.path.basename(ori_img_path)
-        print(f"正在处理: {img_basename}")
+        print(f'=================================')
+        print(f'【ATL-LOG】当前要合并的大图为：{img_basename}')
 
         img_ori = gdal.Open(ori_img_path)
 
         h = img_ori.RasterYSize
         w = img_ori.RasterXSize
         c = img_ori.RasterCount
-        
-        print(f'当前大图的尺寸为：({h}, {w}, {c})')
+
+        print(f'【ATL-LOG】当前大图的尺寸为：({h}, {w}, {c})')
+
         
         # 创建一个新的大图，用来存储合并后的结果
         new_big_img = np.zeros((h,w),dtype=np.uint8)
         # "南岗区"
         img_basename_no_suffix = img_basename.split('.')[0]
 
+        # 查找小图文件夹中,包含大图名字的路径
+        path_img_merge_list = []
         for img_patch_path in img_patch_list:
             if img_basename_no_suffix in img_patch_path:
-               # 'S2_SR_2019_黑龙江省_双鸭山市_宝清县' in 'S2_SR_2019_黑龙江省_双鸭山市_宝清县_0_0_5000.png' 
-                
+                path_img_merge_list.append(img_patch_path)
+            
+
+
+        for patch_path in path_img_merge_list:
+            
+            if os.path.basename(patch_path).split('.')[0] == img_basename_no_suffix:
+                print(f'小图没有序号后缀, {img_basename} 有边长小于 {crop_size}, 未进行裁切, 不需要合并')
+                pass
+
+            else: 
                 img_patch = np.array(Image.open(img_patch_path))
-          
+            
                 # 处理左上的
                 if (not 'xia' in img_patch_path) and (not 'you' in img_patch_path) and (not 'youxia' in img_patch_path):
                     img_h = int(os.path.basename(img_patch_path).split('_')[-3]) # [1]
